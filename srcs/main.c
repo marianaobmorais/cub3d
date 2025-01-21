b9e47f5e86fcff79ea47d1763d4957b2c75d0352
Mariana Morais <141265676+marianaobmorais@users.noreply.github.com>
2025-01-21 11:14:27 +0000

    Added hooks to main

--- srcs/main.c
+++ srcs/main.c
@@ -1,10 +1,20 @@
-# include "../includes/cub3D.h"
+# include "../includes/cub3d.h"
 
 int	main(int argc, char **argv)
 {
 	(void)argc;
 	(void)argv;
+	t_game	*game;
 
-	printf("cub3D\n");
+	printf("cub3D\n"); //debug
+	//argv check
+	//map check
+	game = ft_init_game(/* t_map *map */);
+	if (!game)
+		return (1); //error handler
+	//render
+	mlx_hook(game->window, KeyPress, KeyPressMask, ft_key_input, game);
+	mlx_hook(game->window, DestroyNotify, NoEventMask, ft_close_window, game);
+	mlx_loop(game->mlx);
 	return (0);
 }
