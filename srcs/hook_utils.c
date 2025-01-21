a86e3fa670411f203af9e51d3b7ec458f4fad82b
Mariana Morais <141265676+marianaobmorais@users.noreply.github.com>
2025-01-21 11:13:33 +0000

    Added hook_utils

---
+++ srcs/hook_utils.c
@@ -0,0 +1,23 @@
+#include "../includes/cub3d.h"
+
+int	ft_close_window(t_game *game)
+{
+	(void)game;
+	//free all the things;
+	exit(0);
+}
+
+int	ft_key_input(int keysym, t_game *game)
+{
+	if (keysym == XK_Escape)
+		ft_close_window(game);
+/* 	if (keysym == XK_A || keysym == XK_a)
+		; //left
+	if (keysym == XK_D || keysym == XK_d)
+		; //right
+	if (keysym == XK_W || keysym == XK_w)
+		; //up
+	if (keysym == XK_S || keysym == XK_s)
+		; //down */
+	return (0);
+}
