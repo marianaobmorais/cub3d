d7a172a203e40474b61c3efbbd8f71afd002a1e6
Mariana Oliveira Braga De Morais <mariaoli@c2r6s7.42porto.com>
2025-01-23 18:27:59 +0000

    Added temp map to main

--- srcs/main.c
+++ srcs/main.c
@@ -6,12 +6,25 @@ int	main(int argc, char **argv)
 	(void)argv;
 	t_game	*game;
 
-	printf("cub3D\n"); //debug
 	//argv check
 	//map check
+
+	char *map[] =
+	{
+		"111111111111",
+		"100000000001",
+		"100000001101",
+		"100000001101",
+		"100110000001",
+		"100111000001",
+		"100000000001",
+		"111111111111"
+	}; //temp
+
 	game = ft_init_game(/* t_map *map */);
 	if (!game)
 		return (1); //error handler
+	printf("game is open\n"); //debug
 	//render
 	mlx_hook(game->window, KeyPress, KeyPressMask, ft_key_input, game);
 	mlx_hook(game->window, DestroyNotify, NoEventMask, ft_close_window, game);
