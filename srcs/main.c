2c17fa9d5e84a31589ffa721635b901c569903c6
Mariana Oliveira Braga De Morais <mariaoli@c2r1s8.42porto.com>
2025-02-02 17:23:00 +0000

    Renamed from game to cub and initialized t_cub to 0, to prevent conditional jump

--- srcs/main.c
+++ srcs/main.c
@@ -3,37 +3,23 @@
 int	main(int argc, char **argv)
 {
 	t_cub	*cub;
-	t_game	*game;
 
 	cub = (t_cub *) malloc(sizeof(t_cub));
 	if (!cub)
-		ft_handle_error("Malloc: t_cub", cub);
+		ft_handle_error("malloc: cub", cub);
 	if (argc != 2)
 		ft_handle_error(strerror(EINVAL), cub);
 	ft_loadmap(argv[1], cub);
-	printf("Starting cub3D...\n");
-
-	// char *map[8] =
-	// {
-	// 	"111111111111",
-	// 	"100000000001",
-	// 	"100000001001",
-	// 	"100000000001",
-	// 	"100110000001",
-	// 	"100111000001",
-	// 	"100000000001",
-	// 	"111111111111"
-	// }; //temp
-
-	game = ft_init_game(/* t_map *map */);
-	if (!game)
+	printf("Starting cub3D...\n"); //debug
+	ft_memset(cub, 0, sizeof(cub));
+	cub = ft_init_game(cub);
+	if (!cub)
 		return (1); //error handler
 	printf("game is open\n"); //debug
-	//render
-	mlx_hook(game->window, KeyPress, KeyPressMask, ft_key_input, game);
-	mlx_hook(game->window, DestroyNotify, NoEventMask, ft_close_window, game);
-	mlx_loop(game->mlx);
-	ft_print_map(cub->map);
+	mlx_hook(cub->window, KeyPress, KeyPressMask, ft_key_input, cub);
+	mlx_hook(cub->window, DestroyNotify, NoEventMask, ft_close_window, cub);
+	mlx_loop(cub->mlx);
+	ft_print_map(cub->map); //debug
 	ft_clean_cub(cub);
 	return (0);
 }
