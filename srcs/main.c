718c714812eee083cfd25a3a765907fe70c64e77
Mariana Oliveira Braga De Morais <mariaoli@c1r16s2.42porto.com>
2025-02-12 19:08:20 +0000

    Added ft_minimap_init

--- srcs/main.c
+++ srcs/main.c
@@ -1,5 +1,31 @@
 #include "../includes/cub3d.h"
 
+void ft_minimap_init(t_cub *cub)
+{
+	int		i;
+	t_image	*img;
+
+	i = 0;
+	img = (t_image *)malloc(sizeof(t_image));
+	if (!img)
+		ft_handle_error("malloc: img", cub);
+	img->img_ptr = mlx_new_image(cub->mlx, WIDTH / 7, HEIGHT / 7);
+	if (!img->img_ptr)
+		ft_handle_error("malloc: img->img_ptr", cub);
+	img->bpp = 0;
+	img->line_len = 0;
+	img->endian = 0;
+	img->addr = mlx_get_data_addr(img->img_ptr, &img->bpp, &img->line_len, &img->endian);
+	if (!img->addr)
+		ft_handle_error("malloc: img->addr", cub);
+	// while (i <= HEIGHT / 10)
+	// {
+	// 	ft_put_pixel(img, 0, i, 0xf00edd);
+	// 	i++;
+	// }
+	mlx_put_image_to_window(cub->mlx, cub->window, img->img_ptr, 0, 0); //dar free depois
+}
+
 int	main(int argc, char **argv)
 {
 	t_cub	*cub;
@@ -17,6 +43,9 @@ int	main(int argc, char **argv)
 		return (1); //error handler
 	printf("game is open\n"); //debug
 	ft_print_map(cub->map); //debug
+
+	ft_minimap_init(cub);
+
 	mlx_hook(cub->window, KeyPress, KeyPressMask, ft_key_input, cub);
 	mlx_hook(cub->window, DestroyNotify, NoEventMask, ft_close_window, cub);
 	mlx_loop(cub->mlx);
