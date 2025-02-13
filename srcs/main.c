de4b2d67635d38cd85ecc3008d2d2290eb3dd52c
Mariana Oliveira Braga De Morais <mariaoli@c2r5s8.42porto.com>
2025-02-13 16:44:17 +0000

    Refactored main

--- srcs/main.c
+++ srcs/main.c
@@ -4,18 +4,17 @@ int	main(int argc, char **argv)
 {
 	t_cub	*cub;
 
+	cub = NULL;
+	if (argc != 2)
+		ft_handle_error(strerror(EINVAL), cub);
 	cub = (t_cub *) malloc(sizeof(t_cub));
 	if (!cub)
 		ft_handle_error("malloc: cub", cub);
-	if (argc != 2)
-		ft_handle_error(strerror(EINVAL), cub);
+	//ft_memset(cub, 0, sizeof(cub)); //not sure if it's needed
 	ft_loadmap(argv[1], cub);
-	//printf("Starting cub3D...\n"); //debug
-	ft_memset(cub, 0, sizeof(cub));
 	cub = ft_init_game(cub);
 	if (!cub)
 		return (1); //error handler
-	//printf("game is open\n"); //debug
 	ft_print_map(cub->map); //debug
 	mlx_hook(cub->window, KeyPress, KeyPressMask, ft_key_input, cub);
 	mlx_hook(cub->window, DestroyNotify, NoEventMask, ft_close_window, cub);
