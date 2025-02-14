2b051e10f54dbf77291e267cb45100adebcc0b1d
Jose Junior <jose_evilasio@outlook.com>
2025-02-14 22:25:53 +0000

    added ft to move player around the map

--- srcs/hook_utils.c
+++ srcs/hook_utils.c
@@ -6,17 +6,33 @@ int	ft_close_window(t_cub *cub)
 	exit(0);
 }
 
+void	ft_player_motion(int y, int x, t_cub *cub)
+{
+	int	old_x;
+	int	old_y;
+
+	old_x = cub->map->player_pos_x;
+	old_y = cub->map->player_pos_y;
+	if (cub->map->matrix[x][y] == '1')
+		return ;
+	cub->map->player_pos_x = x;
+	cub->map->player_pos_y = y;
+	cub->map->matrix[old_x][old_y] = '0';
+	cub->map->matrix[x][y] = 'S';
+	ft_print_map(cub->map); //debug
+}
+
 int	ft_key_input(int keysym, t_cub *cub)
 {
 	if (keysym == XK_Escape)
 		ft_close_window(cub);
 	if (keysym == XK_A || keysym == XK_a)
-		; //left
+		ft_player_motion(cub->map->player_pos_y - 1, cub->map->player_pos_x, cub); //left
 	if (keysym == XK_D || keysym == XK_d)
-		; //right
+		ft_player_motion(cub->map->player_pos_y + 1, cub->map->player_pos_x, cub); //right
 	if (keysym == XK_W || keysym == XK_w)
-		; //up
+		ft_player_motion(cub->map->player_pos_y, cub->map->player_pos_x - 1, cub); //up
 	if (keysym == XK_S || keysym == XK_s)
-		; //down
+		ft_player_motion(cub->map->player_pos_y, cub->map->player_pos_x + 1, cub); //down
 	return (0);
 }
