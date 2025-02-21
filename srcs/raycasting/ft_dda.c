fb6e70597b0b5773f6bccf0b20734dae671f1f2f
Mariana <mariana.obm@gmail.com>
2025-02-21 12:37:19 +0000

    Working on ft_dda()

--- srcs/raycasting/ft_dda.c
+++ srcs/raycasting/ft_dda.c
@@ -1,20 +1,15 @@
 #include "../../includes/cub3d.h"
 
-void	ft_dda(t_raycast *raycast)
+bool	ft_dda(t_raycast *ray)
 {
-	//These store the distance from the player's position to the next grid line in both X and Y directions.
-	// double	dist_to_x;
-	// double	dist_to_y;
-	//store how far the ray has to travel in world space to cross one grid line in the X or Y direction.
-	// double	delta_dist_x;
-	// double	delta_dist_y;
-	//This will hold the final distance from the player to the wall along the ray. Used for rendering the correct height of the wall in perspective projection.
-	// double	perp_wall_dist;
-	//stepX and stepY determine which direction the ray moves in each axis.
-	//If rayDirX > 0, the ray moves right (stepX = +1), otherwise, it moves left (stepX = -1).
-	//If rayDirY > 0, the ray moves down (stepY = +1), otherwise, it moves up (stepY = -1).
-	// int	step_x;
-	// int	step_y;
+	bool	is_wall;
+
+	is_wall = false;
+	if (ray->dist_to_x < ray->dist_to_y)
+	{
+		ray->dist_to_x += ray->delta_dist.x;
+
+	}
 
 	return ;
 }
