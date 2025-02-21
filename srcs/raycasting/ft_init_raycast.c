4aa87ce9defbb7947e81ab3b42db103e578bb65b
Mariana <mariana.obm@gmail.com>
2025-02-21 12:34:08 +0000

    Added player_squ to raycat struct

--- srcs/raycasting/ft_init_raycast.c
+++ srcs/raycasting/ft_init_raycast.c
@@ -1,7 +1,25 @@
 #include "../../includes/cub3d.h"
 
+// typedef struct s_raycast
+// {
+// 	t_dpoint	player_pos;
+// 	t_dpoint	player_dir;
+// 	t_dpoint	camera_plane;
+// 	t_dpoint	ray_dir;
+// 	t_dpoint	delta_dist;
+// 	t_ipoint	player_squ;
+// 	t_ipoint	step;
+// 	double		camera_curr_x;
+// 	double		frame_time;
+// 	double		last_frame_time;
+// 	double		dist_to_x;
+// 	double		dist_to_y;
+// }	t_raycast;
+
 void	ft_init_raycast(t_map *map, t_raycast *raycast)
 {
+	raycast->player_squ.x = map->player_squ_x;
+	raycast->player_squ.y = map->player_squ_y;
 	raycast->player_pos.x = (double)map->player_squ_x + 0.5;
 	raycast->player_pos.y = (double)map->player_squ_y + 0.5;
 	if (map->direction == NORTH)
