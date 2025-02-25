dbb00873ebf9e660f504fd6d17bc858a58361eef
Mariana <mariana.obm@gmail.com>
2025-02-25 22:15:54 +0000

    Debugging ft_render_walls

--- srcs/raycasting/ft_render_walls.c
+++ srcs/raycasting/ft_render_walls.c
@@ -8,7 +8,7 @@
 //dist_to_x and dist_to_t store the distance from the player's position to the next grid line in both X and Y directions.
 void	ft_get_ray_info(t_raycast *ray, int x)
 {
-	ray->factor = 2 * ((double)x / WIDTH) - 1;
+	ray->factor = 2 * ((double)x / WIDTH) - 1;// or x / (double)WIDTH?
 	ray->ray_dir.x = ray->player_dir.x + (ray->camera_plane.x * ray->factor);
 	ray->ray_dir.y = ray->player_dir.y + (ray->camera_plane.y * ray->factor);
 	if (ray->ray_dir.x == 0)
@@ -19,6 +19,7 @@ void	ft_get_ray_info(t_raycast *ray, int x)
 		ray->delta_dist.y = INT_MAX;
 	else
 		ray->delta_dist.y = fabs(1 / ray->ray_dir.y);
+
 	if (ray->ray_dir.x < 0)
 		ray->dist_to_x = (ray->player_pos.x - (double)ray->player_squ.x) * ray->delta_dist.x;
 	else
@@ -51,18 +52,24 @@ void	ft_get_wall_height(t_raycast *ray)
 		ray->perp_wall_dist = ray->dist_to_x - ray->delta_dist.x;
 	else
 		ray->perp_wall_dist = ray->dist_to_y - ray->delta_dist.y;
+	// if (ray->hit_side == EAST || ray->hit_side == WEST)
+	// 	ray->perp_wall_dist = (ray->player_squ.x - ray->player_pos.x + (1 - ray->step.x) / 2) / ray->ray_dir.x;
+	// else
+	// 	ray->perp_wall_dist = (ray->player_squ.y - ray->player_pos.y + (1 - ray->step.y) / 2) / ray->ray_dir.y;
+
 	//calculate the height of the line that has to be drawn on screen: this 
 	//is the inverse of perpWallDist, and then multiplied by h, the height in 
 	//pixels of the screen, to bring it to pixel coordinates. You can of course
 	//also multiply it with another value, for example 2*h, if you want to walls
 	//to be higher or lower. The value of h will make the walls look like cubes with 
 	ray->wall_height = (int)(HEIGHT / ray->perp_wall_dist);
-	ray->wall_start = -ray->wall_height / 2 + HEIGHT / 2;
+	ray->wall_start = -(ray->wall_height / 2) + (HEIGHT / 2);
 	if (ray->wall_start < 0)
 		ray->wall_start = 0;
-	ray->wall_end = ray->wall_height / 2 + HEIGHT / 2;
+	ray->wall_end = (ray->wall_height / 2) + (HEIGHT / 2);
 	if (ray->wall_end >= HEIGHT)
 		ray->wall_end = HEIGHT - 1;
+	printf("Ray %d: perp_wall_dist = %f, wall_height = %d\n", x, ray->perp_wall_dist, ray->wall_height); //debug
 }
 
 void	ft_paint_ray(t_cub *cub, int x, int color)
@@ -70,9 +77,9 @@ void	ft_paint_ray(t_cub *cub, int x, int color)
 	int	h;
 
 	h = cub->raycast->wall_start;
-	printf("wall start %d\n", cub->raycast->wall_start); //debug
-	printf("wall end %d\n", cub->raycast->wall_end); //debug
-	printf("wall height %d\n", cub->raycast->wall_height); //debug
+	// printf("wall start %d\n", cub->raycast->wall_start); //debug
+	// printf("wall end %d\n", cub->raycast->wall_end); //debug
+	// printf("wall height %d\n", cub->raycast->wall_height); //debug
 	while (h <= cub->raycast->wall_end)
 	{
 		ft_put_pixel(cub->image, x, h, color);
