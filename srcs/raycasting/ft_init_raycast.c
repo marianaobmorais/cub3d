dca5c56d4309af33b17e885d1385b603d3422ba9
Mariana <mariana.obm@gmail.com>
2025-02-25 21:28:39 +0000

    Refactored ft_init_raycast()

--- srcs/raycasting/ft_init_raycast.c
+++ srcs/raycasting/ft_init_raycast.c
@@ -1,20 +1,36 @@
 #include "../../includes/cub3d.h"
 
-// typedef struct s_raycast
-// {
-// 	t_dpoint	player_pos;
-// 	t_dpoint	player_dir;
-// 	t_dpoint	camera_plane;
-// 	t_dpoint	ray_dir;
-// 	t_dpoint	delta_dist;
-// 	t_ipoint	player_squ;
-// 	t_ipoint	step;
-// 	double		camera_curr_x;
-// 	double		frame_time;
-// 	double		last_frame_time;
-// 	double		dist_to_x;
-// 	double		dist_to_y;
-// }	t_raycast;
+static void	ft_init_north(t_raycast *raycast)
+{
+	raycast->player_dir.x = 0;
+	raycast->player_dir.y = -1;
+	raycast->camera_plane.x = 0.66;
+	raycast->camera_plane.y = 0;
+}
+
+static void	ft_init_south(t_raycast *raycast)
+{
+	raycast->player_dir.x = 0;
+	raycast->player_dir.y = 1;
+	raycast->camera_plane.x = -0.66;
+	raycast->camera_plane.y = 0;
+}
+
+static void	ft_init_east(t_raycast *raycast)
+{
+	raycast->player_dir.x = 1;
+	raycast->player_dir.y = 0;
+	raycast->camera_plane.x = 0;
+	raycast->camera_plane.y = 0.66;
+}
+
+static void	ft_init_west(t_raycast *raycast)
+{
+	raycast->player_dir.x = -1;
+	raycast->player_dir.y = 0;
+	raycast->camera_plane.x = 0;
+	raycast->camera_plane.y = -0.66;
+}
 
 void	ft_init_raycast(t_cub *cub)
 {
@@ -26,33 +42,13 @@ void	ft_init_raycast(t_cub *cub)
 	cub->raycast->player_pos.x = (double)cub->map->player_squ_x + 0.5;
 	cub->raycast->player_pos.y = (double)cub->map->player_squ_y + 0.5;
 	if (cub->map->direction == NORTH)
-	{
-		cub->raycast->player_dir.x = 0;
-		cub->raycast->player_dir.y = -1;
-		cub->raycast->camera_plane.x = 0.66;
-		cub->raycast->camera_plane.y = 0;
-	}
+		ft_init_north(cub->raycast);
 	if (cub->map->direction == SOUTH)
-	{
-		cub->raycast->player_dir.x = 0;
-		cub->raycast->player_dir.y = 1;
-		cub->raycast->camera_plane.x = -0.66;
-		cub->raycast->camera_plane.y = 0;
-	}
+		ft_init_south(cub->raycast);
 	if (cub->map->direction == EAST)
-	{
-		cub->raycast->player_dir.x = 1;
-		cub->raycast->player_dir.y = 0;
-		cub->raycast->camera_plane.x = 0;
-		cub->raycast->camera_plane.y = 0.66;
-	}
+		ft_init_east(cub->raycast);
 	if (cub->map->direction == WEST)
-	{
-		cub->raycast->player_dir.x = -1;
-		cub->raycast->player_dir.y = 0;
-		cub->raycast->camera_plane.x = 0;
-		cub->raycast->camera_plane.y = -0.66;
-	}
+		ft_init_west(cub->raycast);
 	cub->raycast->frame_time = 0;
 	cub->raycast->last_frame_time = 0;
 }
