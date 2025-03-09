cda98ad9537baa78e52363f9ee35a24022435093
Mariana Oliveira Braga De Morais <mariaoli@c2r6s3.42porto.com>
2025-03-09 18:24:55 +0000

    Added other inits to ft_init_structs

--- srcs/ft_init_structs.c
+++ srcs/ft_init_structs.c
@@ -1,16 +1,27 @@
 
 #include "../includes/cub3d.h"
 
-t_cub	*ft_init_structs(t_cub *cub)
+t_cub	*ft_init_structs(t_cub *cub, char *argv)
 {
-	//add the other inits here?
 	cub = (t_cub *) malloc(sizeof(t_cub));
 	if (!cub)
 		ft_handle_error("malloc: cub", cub);
+	ft_load_map(argv, cub);
 	cub->mlx = NULL;
 	cub->window = NULL;
 	cub->image = NULL;
-	cub->minimap = NULL;
+	//cub->minimap = NULL;
 	cub->raycast = NULL;
+	ft_init_raycast(cub);
+	cub->mlx = mlx_init();
+	if (!cub->mlx)
+		ft_handle_error("malloc: cub->mlx", cub);
+	cub->window = mlx_new_window(cub->mlx, WIDTH, HEIGHT, "cub3d");
+	if (!cub->window)
+		ft_handle_error("malloc: cub->window", cub);
+	cub->image = (t_image *)malloc(sizeof(t_image));
+	if (!cub->image)
+		ft_handle_error("malloc: cub->image", cub);
+	cub->image->img_ptr = NULL;
 	return (cub);
 }
