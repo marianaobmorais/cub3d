feafa242d2c1fe3eb1f8b706090c892f6ae76ea1
Mariana Oliveira Braga De Morais <mariaoli@c1r11s1.42porto.com>
2025-03-17 19:27:35 +0000

    Refactored ft_key_input and added ft_rotate

--- srcs/hook_utils.c
+++ srcs/hook_utils.c
@@ -6,7 +6,7 @@ int	ft_close_window(t_cub *cub)
 	exit(0);
 }
 
-void	test(t_cub *cub, double angle)
+void	ft_rotate(t_cub *cub, double angle)
 {
 	double	old_dir_x;
 	double	old_plane_x;
@@ -19,191 +19,61 @@ void	test(t_cub *cub, double angle)
 	cub->raycast->camera_plane.y = old_plane_x * sin(angle) + cub->raycast->camera_plane.y * cos(angle);
 }
 
+void	ft_move_left(t_cub *cub, double *tmp_x, double *tmp_y)
+{
+	*tmp_x -= cub->raycast->camera_plane.x * cub->raycast->move_speed;
+	*tmp_y -= cub->raycast->camera_plane.y * cub->raycast->move_speed;
+}
+
+void	ft_move_right(t_cub *cub, double *tmp_x, double *tmp_y)
+{
+	*tmp_x += cub->raycast->camera_plane.x * cub->raycast->move_speed;
+	*tmp_y += cub->raycast->camera_plane.y * cub->raycast->move_speed;
+}
+
+void	ft_move_up(t_cub *cub, double *tmp_x, double *tmp_y)
+{
+	*tmp_x += cub->raycast->player_dir.x * cub->raycast->move_speed;
+	*tmp_y += cub->raycast->player_dir.y * cub->raycast->move_speed;
+}
+
+void	ft_move_down(t_cub *cub, double *tmp_x, double *tmp_y)
+{
+	*tmp_x -= cub->raycast->player_dir.x * cub->raycast->move_speed;
+	*tmp_y -= cub->raycast->player_dir.y * cub->raycast->move_speed;
+}
+
+void	ft_update_position(t_cub *cub, double tmp_x, double tmp_y)
+{
+	cub->raycast->player_pos.x = tmp_x;
+	cub->raycast->player_pos.y = tmp_y;
+	cub->raycast->player_squ.x = (int)tmp_x;
+	cub->raycast->player_squ.y = (int)tmp_y;
+}
+
 int	ft_key_input(int keysym, t_cub *cub)
 {
 	double	tmp_x;
 	double	tmp_y;
 
-	tmp_x = cub->raycast->player_pos.x;
-	tmp_y = cub->raycast->player_pos.y;
 	if (keysym == XK_Escape)
 		ft_close_window(cub);
+	tmp_x = cub->raycast->player_pos.x;
+	tmp_y = cub->raycast->player_pos.y;
 	if (keysym == XK_A || keysym == XK_a)
-	{
-		//left
-		//printf("pressed a\n"); //debug
-		tmp_x -= cub->raycast->camera_plane.x * cub->raycast->move_speed;
-		tmp_y -= cub->raycast->camera_plane.y * cub->raycast->move_speed;
-		//printf("tmp_x = %lf, tmp_y = %lf\n", tmp_x, tmp_y); //debug
-		//printf("cub->map->matrix[(int)tmp_x][(int)tmp_y] = %c\n", cub->map->matrix[(int)tmp_x][(int)tmp_y]); //debug
-		if (cub->map->matrix[(int)tmp_x][(int)tmp_y] != '1')
-		{
-			cub->raycast->player_pos.x = tmp_x;
-			cub->raycast->player_pos.y = tmp_y;
-			cub->raycast->player_squ.x = floor(tmp_x);
-			cub->raycast->player_squ.y =  floor(tmp_y);
-		}
-		//printf("player_pos_x = %lf, player_pos_y = %lf\n", cub->raycast->player_pos.x, cub->raycast->player_pos.y); //debug
-	}
+		ft_move_left(cub, &tmp_x, &tmp_y);
 	if (keysym == XK_D || keysym == XK_d)
-	{
-		//right
-		//printf("pressed d\n");//debug
-		tmp_x += cub->raycast->camera_plane.x * cub->raycast->move_speed;
-		tmp_y += cub->raycast->camera_plane.y * cub->raycast->move_speed;
-		//printf("tmp_x = %lf, tmp_y = %lf\n", tmp_x, tmp_y); //debug
-		//printf("cub->map->matrix[(int)tmp_x][(int)tmp_y] = %c\n", cub->map->matrix[(int)tmp_x][(int)tmp_y]); //debug
-		if (cub->map->matrix[(int)tmp_x][(int)tmp_y] != '1') //sometimes it show beyond the wall
-		{
-			cub->raycast->player_pos.x = tmp_x;
-			cub->raycast->player_pos.y = tmp_y;
-			cub->raycast->player_squ.x = floor(tmp_x);
-			cub->raycast->player_squ.y =  floor(tmp_y);
-		}
-		//printf("player_pos_x = %lf, player_pos_y = %lf\n", cub->raycast->player_pos.x, cub->raycast->player_pos.y); //debug
-	}
+		ft_move_right(cub, &tmp_x, &tmp_y);
 	if (keysym == XK_W || keysym == XK_w)
-	{
-		//up
-		//printf("pressed w\n");//debug
-		tmp_x += cub->raycast->player_dir.x * cub->raycast->move_speed;
-		tmp_y += cub->raycast->player_dir.y * cub->raycast->move_speed;
-		//printf("tmp_x = %lf, tmp_y = %lf\n", tmp_x, tmp_y); //debug
-		//printf("cub->map->matrix[(int)tmp_x][(int)tmp_y] = %c\n", cub->map->matrix[(int)tmp_x][(int)tmp_y]); //debug
-		if (cub->map->matrix[(int)tmp_x][(int)tmp_y] != '1')
-		{
-			cub->raycast->player_pos.x = tmp_x;
-			cub->raycast->player_pos.y = tmp_y;
-			cub->raycast->player_squ.x = floor(tmp_x);
-			cub->raycast->player_squ.y =  floor(tmp_y);
-		}
-		//printf("player_pos_x = %lf, player_pos_y = %lf\n", cub->raycast->player_pos.x, cub->raycast->player_pos.y); //debug
-	}
+		ft_move_up(cub, &tmp_x, &tmp_y);
 	if (keysym == XK_S || keysym == XK_s)
-	{
-		//down
-		//printf("pressed s\n");//debug
-		tmp_x -= cub->raycast->player_dir.x * cub->raycast->move_speed;
-		tmp_y -= cub->raycast->player_dir.y * cub->raycast->move_speed;
-		//printf("tmp_x = %lf, tmp_y = %lf\n", tmp_x, tmp_y); //debug
-		//printf("cub->map->matrix[(int)tmp_x][(int)tmp_y] = %c\n", cub->map->matrix[(int)tmp_x][(int)tmp_y]); //debug
-		if (cub->map->matrix[(int)tmp_x][(int)tmp_y] != '1')
-		{
-			cub->raycast->player_pos.x = tmp_x;
-			cub->raycast->player_pos.y = tmp_y;
-			cub->raycast->player_squ.x = floor(tmp_x);
-			cub->raycast->player_squ.y =  floor(tmp_y);
-		}
-		//printf("player_pos_x = %lf, player_pos_y = %lf\n", cub->raycast->player_pos.x, cub->raycast->player_pos.y); //debug
-	}
+		ft_move_down(cub, &tmp_x, &tmp_y);
+	if (cub->map->matrix[(int)tmp_x][(int)tmp_y] != '1')
+		ft_update_position(cub, tmp_x, tmp_y);
 	if (keysym == XK_Left)
-	{
-		test(cub, 0.1);
-	}
+		ft_rotate(cub, MOVE_SPEED);
 	if (keysym == XK_Right)
-	{
-		test(cub, -0.1);
-	}
-	if (keysym == XK_Left || keysym == XK_Right|| keysym == XK_A || keysym == XK_a || keysym == XK_D || keysym == XK_d || keysym == XK_W || keysym == XK_w || keysym == XK_S || keysym == XK_s)
-	{
-		//printf("print image\n\n");//debug
-		ft_put_image(cub);
-		//ft_put_minimap(cub);
-	}
+		ft_rotate(cub, -MOVE_SPEED);
+	ft_put_image(cub);
 	return (0);
 }
-
-
-// int	ft_key_input(int keysym, t_cub *cub)
-// {
-// 	int			tmp_pos_x;
-// 	int			tmp_pos_y;
-// 	t_raycast	*ray;
-
-// 	ray = cub->raycast;
-// 	tmp_pos_x = ray->player_pos.x;
-// 	tmp_pos_y = ray->player_pos.y;
-// 	if (keysym == XK_Escape)
-// 		ft_close_window(cub);
-// 	if (keysym == XK_A || keysym == XK_a)
-// 	{
-// 		//left
-// 		printf("pressed a\n");
-// 		tmp_pos_x -= ray->camera_plane.x * ray->move_speed;
-// 		tmp_pos_y -= ray->camera_plane.y * ray->move_speed;
-// 		if (cub->map->matrix[(int)tmp_pos_x][(int)tmp_pos_y] == '0')
-// 		{
-// 			ray->player_pos.x = tmp_pos_x;
-// 			ray->player_pos.y = tmp_pos_y;
-// 		}
-// 		printf("x = %lf, y = %lf\n", ray->player_pos.x, ray->player_pos.y); //debug
-// 	}
-// 	if (keysym == XK_D || keysym == XK_d)
-// 	{
-// 		//right
-// 		printf("pressed d\n");
-// 		tmp_pos_x += ray->camera_plane.x * ray->move_speed;
-// 		tmp_pos_y += ray->camera_plane.y * ray->move_speed;
-// 		if (cub->map->matrix[(int)tmp_pos_x][(int)tmp_pos_y] == '0') //sometimes it show beyond the wall
-// 		{
-// 			ray->player_pos.x = tmp_pos_x;
-// 			ray->player_pos.y = tmp_pos_y;
-// 		}
-// 		printf("x = %lf, y = %lf\n", ray->player_pos.x, ray->player_pos.y); //debug
-// 	}
-// 	if (keysym == XK_W || keysym == XK_w)
-// 	{
-// 		//up
-// 		printf("pressed w\n");//debug
-// 		tmp_pos_x += ray->player_dir.x * ray->move_speed;
-// 		tmp_pos_y += ray->player_dir.y * ray->move_speed;
-// 		if (cub->map->matrix[(int)tmp_pos_x][(int)tmp_pos_y] == '0')
-// 		{
-// 			ray->player_pos.x = tmp_pos_x;
-// 			ray->player_pos.y = tmp_pos_y;
-// 		}
-// 		printf("x = %lf, y = %lf\n", ray->player_pos.x, ray->player_pos.y); //debug
-// 	}
-// 	if (keysym == XK_S || keysym == XK_s)
-// 	{
-// 		//down
-// 		printf("pressed s\n");//debug
-// 		tmp_pos_x -= ray->player_dir.x * ray->move_speed;
-// 		tmp_pos_y -= cub->raycast->player_dir.y * cub->raycast->move_speed;
-// 		if (cub->map->matrix[(int)tmp_pos_x][(int)tmp_pos_y] == '0')
-// 		{
-// 			cub->raycast->player_pos.x = tmp_pos_x;
-// 			cub->raycast->player_pos.y = tmp_pos_y;
-// 		}
-// 		printf("x = %lf, y = %lf\n", cub->raycast->player_pos.x, cub->raycast->player_pos.y); //debug
-// 	}
-
-// 	// double	tmp_dir_x;
-// 	// double	tmp_dir_y;
-// 	// double	tmp_plane_x;
-// 	// double	tmp_plane_y;
-
-// 	// tmp_dir_x = ray->ray_dir.x;
-// 	// tmp_dir_y = ray->ray_dir.y;
-// 	// tmp_plane_x = ray->camera_plane.x;
-// 	// tmp_plane_y = ray->camera_plane.y;
-// 	// if (keysym == XK_Left)
-// 	// {
-// 	// 	//rotate left
-// 	// 	/* ray->ray_dir.x */ = ;
-// 	// }
-// 	// if (keysym == XK_Right)
-// 	// {
-// 	// 	//rotate right
-// 	// 	;
-// 	// }
-// 	// if (keysym == XK_A || keysym == XK_a || keysym == XK_D || keysym == XK_d
-// 	// 	|| keysym == XK_W || keysym == XK_w || keysym == XK_S || keysym == XK_s
-// 	// 	|| keysym == XK_Left || keysym == XK_Right)
-// 	// {
-// 		printf("print image\n"); //debug
-// 		ft_put_image(cub);
-// 		ft_put_minimap(cub);
-// 	//}
-// 	return (0);
-// }
