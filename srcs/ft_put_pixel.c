1c355f65742b3dd32c9edde90728ccd8d7ddf37a
Mariana Oliveira Braga De Morais <mariaoli@c1r12s1.42porto.com>
2025-03-19 20:11:35 +0000

    Initialized offset var to 0

--- srcs/ft_put_pixel.c
+++ srcs/ft_put_pixel.c
@@ -5,6 +5,7 @@ void	ft_put_pixel(t_image *img, int x, int y, int color)
 	//add brief
 	int	offset;
 
+	offset = 0;
 	offset = (y * img->line_len) + (x * (img->bpp / 8));
 	*((unsigned int *)(offset + img->addr)) = color;
 }
