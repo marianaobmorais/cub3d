545339c94e29bbd767d483efdf59102bbd83db57
Mariana Oliveira Braga De Morais <mariaoli@c1r12s1.42porto.com>
2025-03-19 20:12:21 +0000

    Working on ft_init_structure

--- srcs/ft_init_structs.c
+++ srcs/ft_init_structs.c
@@ -12,12 +12,23 @@ void	ft_init_texture(t_cub *cub)
 	ft_memset(&ray->west_texture, 0, sizeof(t_texture));
 	ray->north_texture.img_ptr = mlx_xpm_file_to_image(cub->mlx,
 		cub->map->north_texture, &ray->north_texture.width, &ray->north_texture.height);
+	ray->north_texture.addr = mlx_get_data_addr(ray->north_texture.img_ptr, &ray->north_texture.bpp, &ray->north_texture.line_len, &ray->north_texture.endian);
+	
 	ray->south_texture.img_ptr = mlx_xpm_file_to_image(cub->mlx,
 		cub->map->south_texture, &ray->south_texture.width, &ray->south_texture.height);
+	ray->south_texture.addr = mlx_get_data_addr(ray->south_texture.img_ptr, &ray->south_texture.bpp, &ray->south_texture.line_len, &ray->south_texture.endian);
+
 	ray->east_texture.img_ptr = mlx_xpm_file_to_image(cub->mlx,
 		cub->map->east_texture, &ray->east_texture.width, &ray->east_texture.height);
+	ray->east_texture.addr = mlx_get_data_addr(ray->east_texture.img_ptr, &ray->east_texture.bpp, &ray->east_texture.line_len, &ray->east_texture.endian);
+	
 	ray->west_texture.img_ptr = mlx_xpm_file_to_image(cub->mlx,
 		cub->map->west_texture, &ray->west_texture.width, &ray->west_texture.height);
+	ray->west_texture.addr = mlx_get_data_addr(ray->west_texture.img_ptr, &ray->west_texture.bpp, &ray->west_texture.line_len, &ray->west_texture.endian);
+
+	if (!ray->north_texture.img_ptr || !ray->south_texture.img_ptr
+		|| !ray->east_texture.img_ptr || !ray->west_texture.img_ptr)
+		ft_handle_error("mlx_xpm_file_to_image", cub); //test this later //check addr too?
 }
 
 void	ft_init_image(t_cub *cub)
@@ -45,11 +56,11 @@ t_cub	*ft_init_structs(t_cub *cub, char *argv)
 	cub->mlx = mlx_init();
 	if (!cub->mlx)
 		ft_handle_error("malloc: cub->mlx", cub);
-	cub->window = mlx_new_window(cub->mlx, WIDTH, HEIGHT, "cub3d");
-	if (!cub->window)
-		ft_handle_error("malloc: cub->window", cub);
 	ft_init_image(cub);
 	ft_init_raycast(cub);
 	ft_init_texture(cub);
+	cub->window = mlx_new_window(cub->mlx, WIDTH, HEIGHT, "cub3d");
+	if (!cub->window)
+		ft_handle_error("malloc: cub->window", cub);
 	return (cub);
 }
