3031ecde01254cf8962a8e2ee23ad18d9c195231
Mariana <mariana.obm@gmail.com>
2025-03-20 12:10:14 +0000

    Added mlx_get_data_addr to ft_init_texture()

--- srcs/ft_init_structs.c
+++ srcs/ft_init_structs.c
@@ -12,23 +12,27 @@ void	ft_init_texture(t_cub *cub)
 	ft_memset(&ray->west_texture, 0, sizeof(t_texture));
 	ray->north_texture.img_ptr = mlx_xpm_file_to_image(cub->mlx,
 		cub->map->north_texture, &ray->north_texture.width, &ray->north_texture.height);
-	ray->north_texture.addr = mlx_get_data_addr(ray->north_texture.img_ptr, &ray->north_texture.bpp, &ray->north_texture.line_len, &ray->north_texture.endian);
-	
 	ray->south_texture.img_ptr = mlx_xpm_file_to_image(cub->mlx,
 		cub->map->south_texture, &ray->south_texture.width, &ray->south_texture.height);
-	ray->south_texture.addr = mlx_get_data_addr(ray->south_texture.img_ptr, &ray->south_texture.bpp, &ray->south_texture.line_len, &ray->south_texture.endian);
-
 	ray->east_texture.img_ptr = mlx_xpm_file_to_image(cub->mlx,
 		cub->map->east_texture, &ray->east_texture.width, &ray->east_texture.height);
-	ray->east_texture.addr = mlx_get_data_addr(ray->east_texture.img_ptr, &ray->east_texture.bpp, &ray->east_texture.line_len, &ray->east_texture.endian);
-	
 	ray->west_texture.img_ptr = mlx_xpm_file_to_image(cub->mlx,
 		cub->map->west_texture, &ray->west_texture.width, &ray->west_texture.height);
-	ray->west_texture.addr = mlx_get_data_addr(ray->west_texture.img_ptr, &ray->west_texture.bpp, &ray->west_texture.line_len, &ray->west_texture.endian);
-
 	if (!ray->north_texture.img_ptr || !ray->south_texture.img_ptr
 		|| !ray->east_texture.img_ptr || !ray->west_texture.img_ptr)
-		ft_handle_error("mlx_xpm_file_to_image", cub); //test this later //check addr too?
+		ft_handle_error("texture.img_ptr", cub); //test this later
+
+	ray->north_texture.addr = mlx_get_data_addr(ray->north_texture.img_ptr,
+		&ray->north_texture.bpp, &ray->north_texture.line_len, &ray->north_texture.endian);
+	ray->south_texture.addr = mlx_get_data_addr(ray->south_texture.img_ptr,
+		&ray->south_texture.bpp, &ray->south_texture.line_len, &ray->south_texture.endian);
+	ray->east_texture.addr = mlx_get_data_addr(ray->east_texture.img_ptr,
+		&ray->east_texture.bpp, &ray->east_texture.line_len, &ray->east_texture.endian);
+	ray->west_texture.addr = mlx_get_data_addr(ray->west_texture.img_ptr,
+		&ray->west_texture.bpp, &ray->west_texture.line_len, &ray->west_texture.endian);
+	if (!ray->north_texture.addr || !ray->south_texture.addr
+		|| !ray->east_texture.addr || !ray->west_texture.addr)
+		ft_handle_error("texture.addr", cub); //test this later
 }
 
 void	ft_init_image(t_cub *cub)
@@ -43,7 +47,7 @@ void	ft_init_image(t_cub *cub)
 	cub->image->addr = mlx_get_data_addr(cub->image->img_ptr, &cub->image->bpp,
 		&cub->image->line_len, &cub->image->endian);
 	if (!cub->image->addr)
-		ft_handle_error("malloc: cub->image->addr", cub);
+		ft_handle_error("cub->image->addr", cub);
 }
 
 t_cub	*ft_init_structs(t_cub *cub, char *argv)
