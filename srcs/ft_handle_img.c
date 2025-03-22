2aacbbf18a6871438ef7dbacf6448a4ac0721ef0
Mariana Oliveira Braga De Morais <mariaoli@c1r9s1.42porto.com>
2025-03-22 17:24:52 +0000

    Refactored ft_handle_imag

--- srcs/ft_handle_img.c
+++ srcs/ft_handle_img.c
@@ -6,7 +6,7 @@
 /*   By: mariaoli <mariaoli@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/21 18:36:57 by mariaoli          #+#    #+#             */
-/*   Updated: 2025/03/21 20:50:42 by mariaoli         ###   ########.fr       */
+/*   Updated: 2025/03/22 17:19:05 by mariaoli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,7 +63,7 @@ static void	ft_render_bg(t_image *img, int ceiling_color, int floor_color)
  * @brief Handles the rendering of the game frame.
  *
  * This function updates the background, renders walls, 
- * draws the HUD (if applicable),
+ * draws the HUD (Heads-Up Display) (if applicable),
  * and displays the final image in the game window.
  *
  * @param cub Pointer to the main game structure.
@@ -72,6 +72,10 @@ void	ft_handle_img(t_cub *cub)
 {
 	ft_render_bg(cub->image, cub->map->ceiling_hex, cub->map->floor_hex);
 	ft_render_walls(cub);
-	ft_put_hud(cub); //bonus
+	ft_render_source_on_hud(cub, cub->hud->watch, 0, 0); //bonus
+	ft_render_source_on_hud(cub, cub->hud->viewmodel, 390, 420); //bonus
+	ft_render_source_on_hud(cub, cub->hud->breads, 820, 0); //bonus
+	//ft_door(cub, cub->hud->door, WIDTH / 2 - 0, HEIGHT / 2 - 70); //bonus //move to render_wall?
+	ft_render_minimap_on_hud(cub); //bonus
 	mlx_put_image_to_window(cub->mlx, cub->window, cub->image->img_ptr, 0, 0);
 }
