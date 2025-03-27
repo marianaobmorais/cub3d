0788dc90a5028744393cd5e2a48f5c887d5a4ae3
Jose Junior <jose_evilasio@outlook.com>
2025-03-27 22:31:40 +0000

    generic ft to load xpm

--- bonus/srcs/hud/ft_init_hud_bonus.c
+++ bonus/srcs/hud/ft_init_hud_bonus.c
@@ -154,6 +154,23 @@ void	ft_init_door(t_cub *cub)
 		ft_handle_error("cub->hud->door->addr", cub);
 }
 
+void	ft_init_xpm_image(t_cub *cub, t_image **img, char *path)
+{
+	*img = (t_image *)malloc(sizeof(t_image));
+	if (!*img)
+		ft_handle_error("malloc: img", cub);
+	ft_memset(*img, 0, sizeof(t_image));
+	(*img)->img_ptr = mlx_xpm_file_to_image(cub->mlx, path, 
+		&(*img)->width, &(*img)->height);
+	if (!(*img)->img_ptr)
+		ft_handle_error("mlx_xpm_file_to_image", cub);
+	(*img)->addr = mlx_get_data_addr((*img)->img_ptr, &(*img)->bpp, \
+		&(*img)->line_len, &(*img)->endian);
+	if (!(*img)->addr)
+		ft_handle_error("mlx_get_data_addr", cub);
+}
+
+
 /**
  * @brief Initializes the HUD (Heads-Up Display) for the game.
  *
@@ -175,10 +192,15 @@ void	ft_init_hud(t_cub *cub)
 		ft_handle_error("malloc: cub->hud->img", cub);
 	ft_memset(cub->hud->img, 0, sizeof(t_image));
 	cub->hud->img->img_ptr = NULL;
-	ft_init_watch(cub);
-	ft_init_viewmodel(cub);
-	ft_init_bread(cub);
-	ft_init_empty_bread(cub);
-	ft_init_breadcrumbs(cub);
-	ft_init_door(cub);
+	ft_init_xpm_image(cub, &cub->hud->watch, "assets/hud/casio.xpm");
+	ft_init_xpm_image(cub, &cub->hud->bread, "assets/hud/bread.xpm"); //bag
+	ft_init_xpm_image(cub, &cub->hud->empty_bread, "assets/hud/empty_bread.xpm"); //bag
+	ft_init_xpm_image(cub, &cub->hud->viewmodel, "assets/hud/viewmodel.xpm");
+	ft_init_xpm_image(cub, &cub->hud->breadcrumbs, "assets/hud/breadcrumbs.xpm");
+	//ft_init_watch(cub);
+	// ft_init_viewmodel(cub);
+	// ft_init_bread(cub);
+	// ft_init_empty_bread(cub);
+	// ft_init_breadcrumbs(cub);
+	// ft_init_door(cub);
 }
