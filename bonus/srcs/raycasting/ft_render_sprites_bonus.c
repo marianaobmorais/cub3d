f94eac3f8847d917c3205d0749af6d19dec6a210
Mariana Oliveira Braga De Morais <mariaoli@c1r8s4.42porto.com>
2025-04-05 16:13:12 +0100

    Refactoring

--- bonus/srcs/raycasting/ft_render_sprites_bonus.c
+++ bonus/srcs/raycasting/ft_render_sprites_bonus.c
@@ -1,58 +1,35 @@
 #include "../../includes/cub3d_bonus.h"
 
-static void	ft_get_sprite_info(t_cub *cub, int i)
+static void	ft_get_draw_info(t_sprite *sprite)
+{
+	(*sprite).start_h = -(*sprite).relative_height / 2 + HEIGHT / 2;
+	(*sprite).end_h = (*sprite).relative_height / 2 + HEIGHT / 2;
+	(*sprite).start_w = (*sprite).screen_w - (*sprite).relative_width / 2;
+	(*sprite).end_w = (*sprite).screen_w + (*sprite).relative_width / 2;
+}
+
+static void	ft_draw_sprite(t_cub *cub, t_sprite sprite)
 {
 	//add brief
-	double	relative_x;
-	double	relative_y;
-	double	inv; //inverse_determinant
-	double	transform_w; //move to struct
-	double	transform_z; //move to struct
-	int		screen_w;
-	int		relative_height;
-	int		sprite_start_h;
-	int		sprite_end_h;
-	int		sprite_width;
+	int		tmp_w;
+	int		tmp_h;
+	int		texture_w;
+	int		texture_h;
+	int		color;
 
-	relative_x = cub->map->sprite[i].pos.x - cub->raycast->player_pos.x;
-	relative_y = cub->map->sprite[i].pos.y - cub->raycast->player_pos.y;
-	inv = 1.0 / (cub->raycast->player_dir.x * cub->raycast->camera_plane.y - cub->raycast->player_dir.y * cub->raycast->camera_plane.x);
-	//The camera plane should be used for depth, while player_dir should be used for horizontal position.
-	//transform_x tells us how far the sprite is from the center of the screen
-	transform_w = inv * (-cub->raycast->player_dir.y * relative_x + cub->raycast->player_dir.x * relative_y);
-	//transform_y tells us depth, which affects sprite scaling
-	//transform_y (depth) increases, sprite_screen_w decreases, making the sprite appear smaller
-	//transform_y decreases, sprite_screen_w increases, making the sprite larger
-	transform_z = inv * (cub->raycast->camera_plane.y * relative_x - cub->raycast->camera_plane.x * relative_y);
-	if (transform_z <= 0)
-		return ;
-	//screen_w will give the horizontal position on the screen where the sprite will be placed, based on its relative position to the player and the camera's orientation.
-	screen_w = (int)((WIDTH / 2) * (1 + transform_w / transform_z)); //calculates where the sprite should be positioned on screen
-	// the apparent height and width of the sprite are inversely proportional to transform_z. The larger the value of transform_z, the smaller the sprite appears on the screen and vice-versa
-	relative_height = (int)HEIGHT / transform_z; //do we need the fabs function? //using 'transform_y' instead of the real distance prevents fisheye
-	sprite_width = relative_height;
-	sprite_start_h = -relative_height / 2 + HEIGHT / 2;
-	sprite_end_h = relative_height / 2 + HEIGHT / 2;
-	int		sprite_start_w = screen_w - sprite_width / 2;
-	int		sprite_end_w = screen_w + sprite_width / 2;
-
-
-	//draw sprite
-	int tmp_w = sprite_start_w;
-	while (tmp_w < sprite_end_w)
+	tmp_w = (sprite).start_w;
+	while (tmp_w < (sprite).end_w)
 	{
-		//printf("tmp_w = %d, sprite_width: %d\n", tmp_w, sprite_width);//debug
-		if (tmp_w >= 0 && tmp_w < WIDTH && transform_z < cub->raycast->buffer[tmp_w])
+		if (tmp_w >= 0 && tmp_w < WIDTH && sprite.transform.y < cub->raycast->buffer[tmp_w])
 		{
-			int	texture_w = (int)((tmp_w - sprite_start_w) * (double)cub->map->sprite[i].img_a.width / sprite_width);
-			//printf("texture_w = %d\n", texture_w);//debug		
-			int	tmp_h = sprite_start_h;
-			while (tmp_h < sprite_end_h)
+			texture_w = (int)((tmp_w - (sprite).start_w) * (double)sprite.img.width / sprite.relative_width);
+			tmp_h = (sprite).start_h;
+			while (tmp_h < (sprite).end_h)
 			{
-				int	texture_h = (int)((tmp_h - sprite_start_h) * (double)cub->map->sprite[i].img_a.height / sprite_height);
-				if (tmp_h >= 0 && tmp_h < HEIGHT /* SPRITE_SIZE */)
+				texture_h = (int)((tmp_h - (sprite).start_h) * (double)sprite.img.height / sprite.relative_height);
+				if (tmp_h >= 0 && tmp_h < HEIGHT)
 				{
-					int color = ft_get_pixel_color(&cub->map->sprite[i].img_a, texture_w, texture_h, cub);
+					color = ft_get_pixel_color(&sprite.img, texture_w, texture_h, cub);
 					if (color == IGNORE)
 					{
 						color = ft_get_pixel_color(cub->image, tmp_w, tmp_h, cub);
@@ -66,6 +43,33 @@ static void	ft_get_sprite_info(t_cub *cub, int i)
 		}
 		tmp_w++;
 	}
+}
+
+static int	ft_get_sprite_info(t_cub *cub, t_sprite *sprite)
+{
+	//add brief
+	double	relative_pos_x;
+	double	relative_pos_y;
+	double	inv_det;
+
+	relative_pos_x = (*sprite).pos.x - cub->raycast->player_pos.x;
+	relative_pos_y = (*sprite).pos.y - cub->raycast->player_pos.y;
+	inv_det = 1.0 / (cub->raycast->player_dir.x * cub->raycast->camera_plane.y - cub->raycast->player_dir.y * cub->raycast->camera_plane.x);
+	//The camera plane should be used for depth, while player_dir should be used for horizontal position.
+	//transform_x tells us how far the sprite is from the center of the screen
+	(*sprite).transform.x = inv_det * (-cub->raycast->player_dir.y * relative_pos_x + cub->raycast->player_dir.x * relative_pos_y);
+	//transform_y tells us depth, which affects sprite scaling
+	//transform_y (depth) increases, sprite_screen_w decreases, making the sprite appear smaller
+	//transform_y decreases, sprite_screen_w increases, making the sprite larger
+	(*sprite).transform.y = inv_det * (cub->raycast->camera_plane.y * relative_pos_x - cub->raycast->camera_plane.x * relative_pos_y);
+	if ((*sprite).transform.y <= 0)
+		return (0);
+	//screen_w will give the horizontal position on the screen where the sprite will be placed, based on its relative position to the player and the camera's orientation.
+	(*sprite).screen_w = (int)((WIDTH / 2) * (1 + (*sprite).transform.x / (*sprite).transform.y)); //calculates where the sprite should be positioned on screen
+	// the apparent height and width of the sprite are inversely proportional to transform_z. The larger the value of transform_z, the smaller the sprite appears on the screen and vice-versa
+	(*sprite).relative_height = (int)HEIGHT / (*sprite).transform.y; //do we need the fabs function? //using 'transform_y' instead of the real distance prevents fisheye
+	(*sprite).relative_width = (*sprite).relative_height;
+	return (1);
 }
 
 static void	ft_sort_sprites(t_map *map)
@@ -98,25 +102,27 @@ void	ft_render_sprites(t_cub *cub)
 {
 	//add brief
 	int	i;
-	int	j;
 
 	i = 0;
-	j = 0;
 	while (i < cub->map->sprite_count)
 	{
-		cub->map->sprite[i].dist = (cub->raycast->player_pos.x - cub->map->sprite[i].pos.x) * (cub->raycast->player_pos.x - cub->map->sprite[i].pos.x) + (cub->raycast->player_pos.y - cub->map->sprite[i].pos.y) * (cub->raycast->player_pos.y - cub->map->sprite[i].pos.y);
-		if (j == 1) //should be 3, but temporarily it's 1
-			j = 0;
-		cub->map->sprite[i].img_a = cub->raycast->sprite_a[j];
-		cub->map->sprite[i].img_b = cub->raycast->sprite_b[j]; //review logic
+		cub->map->sprite[i].dist = (cub->raycast->player_pos.x -
+			cub->map->sprite[i].pos.x) * (cub->raycast->player_pos.x -
+			cub->map->sprite[i].pos.x) + (cub->raycast->player_pos.y -
+			cub->map->sprite[i].pos.y) * (cub->raycast->player_pos.y -
+			cub->map->sprite[i].pos.y);
+		cub->map->sprite[i].img = cub->raycast->sprite_a[0];
 		i++;
-		j++;
 	}
 	ft_sort_sprites(cub->map);
 	i = 0;
 	while (i < cub->map->sprite_count)
 	{
-		ft_get_sprite_info(cub, i);
+		if (ft_get_sprite_info(cub, &cub->map->sprite[i]))
+		{
+			ft_get_draw_info(&cub->map->sprite[i]);
+			ft_draw_sprite(cub, cub->map->sprite[i]);
+		}
 		i++;
 	}
 }
