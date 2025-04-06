57a96a631a6925c712a663ae7195b206ad2b8bc6
Mariana <mariana.obm@gmail.com>
2025-04-06 18:42:21 +0100

    Added ft_init_sprites

--- bonus/srcs/raycasting/ft_render_sprites_bonus.c
+++ bonus/srcs/raycasting/ft_render_sprites_bonus.c
@@ -1,13 +1,5 @@
 #include "../../includes/cub3d_bonus.h"
 
-static void	ft_get_draw_info(t_sprite *sprite)
-{
-	(*sprite).start_h = -(*sprite).relative_height / 2 + HEIGHT / 2;
-	(*sprite).end_h = (*sprite).relative_height / 2 + HEIGHT / 2;
-	(*sprite).start_w = (*sprite).screen_w - (*sprite).relative_width / 2;
-	(*sprite).end_w = (*sprite).screen_w + (*sprite).relative_width / 2;
-}
-
 static void	ft_draw_sprite(t_cub *cub, t_sprite sprite)
 {
 	//add brief
@@ -45,6 +37,17 @@ static void	ft_draw_sprite(t_cub *cub, t_sprite sprite)
 	}
 }
 
+static void	ft_get_draw_info(t_sprite *sprite)
+{
+	//add brief
+	(*sprite).relative_height = (int)HEIGHT / (*sprite).transform.y; //using 'transform_y' instead of the real distance prevents fisheye
+	(*sprite).relative_width = (*sprite).relative_height;
+	(*sprite).start_h = -(*sprite).relative_height / 2 + HEIGHT / 2;
+	(*sprite).end_h = (*sprite).relative_height / 2 + HEIGHT / 2;
+	(*sprite).start_w = (*sprite).screen_w - (*sprite).relative_width / 2;
+	(*sprite).end_w = (*sprite).screen_w + (*sprite).relative_width / 2;
+}
+
 static int	ft_get_sprite_info(t_cub *cub, t_sprite *sprite)
 {
 	//add brief
@@ -54,37 +57,75 @@ static int	ft_get_sprite_info(t_cub *cub, t_sprite *sprite)
 
 	relative_pos_x = (*sprite).pos.x - cub->raycast->player_pos.x;
 	relative_pos_y = (*sprite).pos.y - cub->raycast->player_pos.y;
-	inv_det = 1.0 / (cub->raycast->player_dir.x * cub->raycast->camera_plane.y - cub->raycast->player_dir.y * cub->raycast->camera_plane.x);
+	inv_det = 1.0 / (cub->raycast->player_dir.x * cub->raycast->camera_plane.y
+		- cub->raycast->player_dir.y * cub->raycast->camera_plane.x);
 	//The camera plane should be used for depth, while player_dir should be used for horizontal position.
 	//transform_x tells us how far the sprite is from the center of the screen
-	(*sprite).transform.x = inv_det * (-cub->raycast->player_dir.y * relative_pos_x + cub->raycast->player_dir.x * relative_pos_y);
+	(*sprite).transform.x = inv_det * (-cub->raycast->player_dir.y * 
+		relative_pos_x + cub->raycast->player_dir.x * relative_pos_y);
 	//transform_y tells us depth, which affects sprite scaling
 	//transform_y (depth) increases, sprite_screen_w decreases, making the sprite appear smaller
 	//transform_y decreases, sprite_screen_w increases, making the sprite larger
-	(*sprite).transform.y = inv_det * (cub->raycast->camera_plane.y * relative_pos_x - cub->raycast->camera_plane.x * relative_pos_y);
+	(*sprite).transform.y = inv_det * (cub->raycast->camera_plane.y *
+		relative_pos_x - cub->raycast->camera_plane.x * relative_pos_y);
 	if ((*sprite).transform.y <= 0)
 		return (0);
 	//screen_w will give the horizontal position on the screen where the sprite will be placed, based on its relative position to the player and the camera's orientation.
-	(*sprite).screen_w = (int)((WIDTH / 2) * (1 + (*sprite).transform.x / (*sprite).transform.y)); //calculates where the sprite should be positioned on screen
+	(*sprite).screen_w = (int)((WIDTH / 2) * (1 + (*sprite).transform.x /
+		(*sprite).transform.y)); //calculates where the sprite should be positioned on screen
 	// the apparent height and width of the sprite are inversely proportional to transform_z. The larger the value of transform_z, the smaller the sprite appears on the screen and vice-versa
-	(*sprite).relative_height = (int)HEIGHT / (*sprite).transform.y; //do we need the fabs function? //using 'transform_y' instead of the real distance prevents fisheye
-	(*sprite).relative_width = (*sprite).relative_height;
 	return (1);
 }
 
-static void	ft_sort_sprites(t_map *map)
+static void	ft_update_sprites(t_cub *cub)
 {
 	//add brief
-	//the most distant should go first
+	int	i;
+
+	i = 0;
+	if (cub->raycast->time >= 0.4)
+	{
+		while (i < cub->map->sprite_count)
+		{
+			if (cub->map->sprite->status)
+			{
+				cub->map->sprite[i].img = cub->raycast->sprite_a[0];
+				cub->map->sprite->status = false;
+			}
+			else if (!cub->map->sprite->status && !cub->action)
+			{
+				cub->map->sprite[i].img = cub->raycast->sprite_a[1];
+				cub->map->sprite->status = true;
+			}
+			else if (!cub->map->sprite->status && cub->action)
+			{
+				cub->map->sprite[i].img = cub->raycast->sprite_b[0];
+				cub->map->sprite->status = true;
+			}
+			i++;
+		}
+		cub->raycast->time = 0;
+	}
+	cub->raycast->time += 0.016;
+}
+
+static void	ft_sort_sprites(t_map *map, t_raycast *ray)
+{
+	//add brief //the most distant should go first //bubble sort
 	int			i;
 	int			count;
 	t_sprite	tmp;
 
+	i = -1;
+	while (++i < map->sprite_count)
+		map->sprite[i].dist = (ray->player_pos.x - map->sprite[i].pos.x) * 
+			(ray->player_pos.x - map->sprite[i].pos.x) + (ray->player_pos.y -
+			map->sprite[i].pos.y) * (rayc->player_pos.y - map->sprite[i].pos.y);
 	count = map->sprite_count;
 	while (count)
 	{
-		i = 0;
-		while (i < count)
+		i = -1;
+		while (++i < count)
 		{
 			if (map->sprite[i].dist < map->sprite[i + 1].dist)
 			{
@@ -92,7 +133,6 @@ static void	ft_sort_sprites(t_map *map)
 				map->sprite[i] = map->sprite[i + 1];
 				map->sprite[i + 1] = tmp;
 			}
-			i++;
 		}
 		count--;
 	}
@@ -103,18 +143,8 @@ void	ft_render_sprites(t_cub *cub)
 	//add brief
 	int	i;
 
-	i = 0;
-	while (i < cub->map->sprite_count)
-	{
-		cub->map->sprite[i].dist = (cub->raycast->player_pos.x -
-			cub->map->sprite[i].pos.x) * (cub->raycast->player_pos.x -
-			cub->map->sprite[i].pos.x) + (cub->raycast->player_pos.y -
-			cub->map->sprite[i].pos.y) * (cub->raycast->player_pos.y -
-			cub->map->sprite[i].pos.y);
-		//cub->map->sprite[i].img = cub->raycast->sprite_a[0];
-		i++;
-	}
-	ft_sort_sprites(cub->map);
+	ft_sort_sprites(cub->map, cub->raycast);
+	ft_update_sprites(cub);
 	i = 0;
 	while (i < cub->map->sprite_count)
 	{
