7887a6c5035266f8da28099b347e2d32032cfcc1
Mariana Oliveira Braga De Morais <mariaoli@c1r13s5.42porto.com>
2025-04-07 15:53:59 +0100

    Fixed norm

--- bonus/srcs/raycasting/ft_render_sprites_bonus.c
+++ bonus/srcs/raycasting/ft_render_sprites_bonus.c
@@ -1,35 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_render_sprites_bonus.c                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mariaoli <mariaoli@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/07 15:40:10 by mariaoli          #+#    #+#             */
+/*   Updated: 2025/04/07 15:47:09 by mariaoli         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../../includes/cub3d_bonus.h"
 
-static void	ft_draw_sprite(t_cub *cub, t_sprite sprite, int w)
-{
-	//add brief
-	int		h;
-	int		texture_w;
-	int		texture_h;
-	int		color;
-
-	texture_w = (int)((w - sprite.start_w) * (double)sprite.img.width /
-		sprite.relative_width);
-	h = (sprite).start_h;
-	while (h < sprite.end_h)
-	{
-		texture_h = (int)((h - sprite.start_h) * (double)sprite.img.height /
-			sprite.relative_height);
-		if (h >= 0 && h < HEIGHT)
-		{
-			color = ft_get_pixel_color(&sprite.img, texture_w, texture_h, cub);
-			if (color == IGNORE)
-				color = ft_get_pixel_color(cub->image, w, h, cub);
-			ft_put_pixel(cub->image, w, h, color);
-		}
-		h++;
-	}
-}
-
+/**
+ * @brief Calculates the on-screen size and position of the sprite.
+ *
+ * Based on the sprite's depth (distance from camera), this function determines
+ * its height and width, as well as the start and end positions for drawing
+ * on the screen.
+ *
+ * @param sprite Pointer to the sprite whose drawing info will be updated.
+ */
 static void	ft_get_draw_info(t_sprite *sprite)
 {
-	//add brief
-	(*sprite).relative_height = (int)HEIGHT / (*sprite).transform.y; //using 'transform_y' instead of the real distance prevents fisheye
+	(*sprite).relative_height = (int)HEIGHT / (*sprite).transform.y;
 	(*sprite).relative_width = (*sprite).relative_height;
 	(*sprite).start_h = -(*sprite).relative_height / 2 + HEIGHT / 2;
 	(*sprite).end_h = (*sprite).relative_height / 2 + HEIGHT / 2;
@@ -37,38 +31,50 @@ static void	ft_get_draw_info(t_sprite *sprite)
 	(*sprite).end_w = (*sprite).screen_w + (*sprite).relative_width / 2;
 }
 
+/**
+ * @brief Computes sprite position relative to the camera.
+ *
+ * This function transforms the sprite's world coordinates into camera space
+ * using an inverse determinant and calculates its projected screen position and
+ * depth. If the sprite is behind the player (i.e., not visible), the function
+ * returns 0.
+ *
+ * @param cub Pointer to the main game structure.
+ * @param sprite Pointer to the sprite being transformed.
+ * @return Returns 1 if the sprite is in front of the camera, otherwise 0.
+ */
 static int	ft_get_sprite_info(t_cub *cub, t_sprite *sprite)
 {
-	//add brief
 	double	relative_pos_x;
 	double	relative_pos_y;
 	double	inv_det;
 
 	relative_pos_x = (*sprite).pos.x - cub->raycast->player_pos.x;
 	relative_pos_y = (*sprite).pos.y - cub->raycast->player_pos.y;
-	inv_det = 1.0 / (cub->raycast->player_dir.x * cub->raycast->camera_plane.y
+	inv_det = 1.0 / (cub->raycast->player_dir.x * cub->raycast->camera_plane.y \
 		- cub->raycast->player_dir.y * cub->raycast->camera_plane.x);
-	//The camera plane should be used for depth, while player_dir should be used for horizontal position.
-	//transform_x tells us how far the sprite is from the center of the screen
-	(*sprite).transform.x = inv_det * (-cub->raycast->player_dir.y * 
-		relative_pos_x + cub->raycast->player_dir.x * relative_pos_y);
-	//transform_y tells us depth, which affects sprite scaling
-	//transform_y (depth) increases, sprite_screen_w decreases, making the sprite appear smaller
-	//transform_y decreases, sprite_screen_w increases, making the sprite larger
-	(*sprite).transform.y = inv_det * (cub->raycast->camera_plane.y *
-		relative_pos_x - cub->raycast->camera_plane.x * relative_pos_y);
+	(*sprite).transform.x = inv_det * (-cub->raycast->player_dir.y \
+		* relative_pos_x + cub->raycast->player_dir.x * relative_pos_y);
+	(*sprite).transform.y = inv_det * (cub->raycast->camera_plane.y \
+		* relative_pos_x - cub->raycast->camera_plane.x * relative_pos_y);
 	if ((*sprite).transform.y <= 0)
 		return (0);
-	//screen_w will give the horizontal position on the screen where the sprite will be placed, based on its relative position to the player and the camera's orientation.
-	(*sprite).screen_w = (int)((WIDTH / 2) * (1 + (*sprite).transform.x /
-		(*sprite).transform.y)); //calculates where the sprite should be positioned on screen
-	// the apparent height and width of the sprite are inversely proportional to transform_z. The larger the value of transform_z, the smaller the sprite appears on the screen and vice-versa
+	(*sprite).screen_w = (int)((WIDTH / 2) * (1 + (*sprite).transform.x \
+		/ (*sprite).transform.y));
 	return (1);
 }
 
+/**
+ * @brief Updates sprite animations over time by toggling their image state.
+ *
+ * This function swaps between idle, move, or action sprites depending on the
+ * current time counter and the state of the sprite. Used to simulate animation.
+ * Resets the timer if more than 0.4 seconds have passed.
+ *
+ * @param cub Pointer to the main game structure.
+ */
 static void	ft_update_sprites(t_cub *cub)
 {
-	//update brief
 	int	i;
 
 	if (cub->raycast->time >= 0.4)
@@ -96,18 +102,27 @@ static void	ft_update_sprites(t_cub *cub)
 	cub->raycast->time += 0.016;
 }
 
+/**
+ * @brief Sorts the sprites in descending order of distance from the player.
+ *
+ * Implements a simple bubble sort to arrange sprites so that distant ones are
+ * drawn first. This ensures correct rendering with overlapping transparency and
+ * depth effects.
+ *
+ * @param map Pointer to the map structure containing sprites.
+ * @param ray Pointer to the raycasting structure for player position reference.
+ */
 static void	ft_sort_sprites(t_map *map, t_raycast *ray)
 {
-	//add brief //the most distant should go first //bubble sort
 	int			i;
 	int			count;
 	t_sprite	tmp;
 
 	i = -1;
 	while (++i < map->sprite_count)
-		map->sprite[i].dist = (ray->player_pos.x - map->sprite[i].pos.x) * 
-			(ray->player_pos.x - map->sprite[i].pos.x) + (ray->player_pos.y -
-			map->sprite[i].pos.y) * (ray->player_pos.y - map->sprite[i].pos.y);
+		map->sprite[i].dist = (ray->player_pos.x - map->sprite[i].pos.x) \
+		* (ray->player_pos.x - map->sprite[i].pos.x) + (ray->player_pos.y \
+		- map->sprite[i].pos.y) * (ray->player_pos.y - map->sprite[i].pos.y);
 	count = map->sprite_count;
 	while (count)
 	{
@@ -125,9 +140,17 @@ static void	ft_sort_sprites(t_map *map, t_raycast *ray)
 	}
 }
 
+/**
+ * @brief Renders all visible sprites in the scene.
+ *
+ * This function sorts and updates sprite animations, calculates their on-screen
+ * positions and sizes, and draws them column by column while respecting depth
+ * buffering to ensure correct layering with walls and other sprites.
+ *
+ * @param cub Pointer to the main game structure.
+ */
 void	ft_render_sprites(t_cub *cub)
 {
-	//add brief
 	int	i;
 	int	w;
 
@@ -142,8 +165,8 @@ void	ft_render_sprites(t_cub *cub)
 			w = cub->map->sprite[i].start_w;
 			while (w < cub->map->sprite[i].end_w)
 			{
-				if (w >= 0 && w < WIDTH && cub->map->sprite[i].transform.y <
-						cub->raycast->buffer[w])
+				if (w >= 0 && w < WIDTH && cub->map->sprite[i].transform.y \
+						< cub->raycast->buffer[w])
 					ft_draw_sprite(cub, cub->map->sprite[i], w);
 				w++;
 			}
